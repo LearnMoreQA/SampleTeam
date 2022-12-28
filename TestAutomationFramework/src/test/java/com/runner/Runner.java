19e8357bc55e33a5240d5cafb2f24f7030de669d
bearerto <121045331+bearerto@users.noreply.github.com>
2022-12-28 08:18:03 +0530

    Plugin Updated

--- TestAutomationFramework/src/test/java/com/runner/Runner.java
+++ TestAutomationFramework/src/test/java/com/runner/Runner.java
@@ -1,17 +1,15 @@
 package com.runner;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/com.featurefile"},
+        features = {"src/test/resources/com/featurefile"},
         glue = {"com.stepdefinition"},
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         tags = "@login"
 )
-public class Runner {
+public class Runner extends AbstractTestNGCucumberTests {
 
 }
